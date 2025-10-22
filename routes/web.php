29cafa704222f0080654a8fb92bc73fc46c7616d
Nguyen Huynh Phuc <nguyenhuynhphuc1210@gmail.com>
2025-10-22 14:15:25 +0700

    Commit 1

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,3 @@ use Illuminate\Support\Facades\Mail;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test-mail', function () {
-    try {
-        Mail::raw('Email test từ Laravel trên Render', function ($message) {
-            $message->to('nguyenhuynhphuc1210@gmail.com')
-                    ->subject('Test Email Laravel Render');
-        });
-
-        return 'Email đã gửi thành công!';
-    } catch (\Exception $e) {
-        return 'Lỗi gửi email: ' . $e->getMessage();
-    }
-});
